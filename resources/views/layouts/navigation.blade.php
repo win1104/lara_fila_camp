f10e74cdca7f5f21f247754bae0b6757d4e927a9
Win1104 <win1104@gmail.com>
2025-05-08 18:30:59 +0800

    調整 header 元件

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,89 +2,113 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
 
 
 
 
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
-                        {{ __('Chirps') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('note.index')" :active="request()->routeIs('note.index')">
-                        {{ __('Note') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('openai.index')" :active="request()->routeIs('openai.index')">
-                        {{ __('OPEN AI') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('gpt.index')" :active="request()->routeIs('gpt.index')">
-                        {{ __('Chat GTP') }}
-                    </x-nav-link>
-                </div>
-            </div>
 
 
 
+                <!-- maryUI -->
+                <div class="navbar bg-base-100 shadow-sm">
 
-            <!-- maryUI -->
-            <div class="navbar bg-base-100 shadow-sm">
-                <div class="flex-1">
-                    <a class="btn btn-ghost text-xl">daisyUI</a>
-                </div>
-                <div class="flex-none">
-                    <div class="dropdown dropdown-end">
-                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-                        <div class="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
-                        <span class="badge badge-sm indicator-item">8</span>
-                        </div>
-                    </div>
-                    <div
-                        tabindex="0"
-                        class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
-                        <div class="card-body">
-                        <span class="text-lg font-bold">8 Items</span>
-                        <span class="text-info">Subtotal: $999</span>
-                        <div class="card-actions">
-                            <button class="btn btn-primary btn-block">View cart</button>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="dropdown dropdown-end">
-                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-                        <div class="w-10 rounded-full">
-                        <img
-                            alt="Tailwind CSS Navbar component"
-                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                        </div>
-                    </div>
-                    <ul
-                        tabindex="0"
-                        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li>
-                        <a class="justify-between">
-                            Profile
-                            <span class="badge">New</span>
+
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <a href="{{ route('home') }}">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                         </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
+                    </div>
+
+
+
+                    <div class="flex-1">
+
+
+
+                        <!-- Navigation Links -->
+                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                                {{ __('Dashboard') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                                {{ __('Chirps') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('note.index')" :active="request()->routeIs('note.index')">
+                                {{ __('Note') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('openai.index')" :active="request()->routeIs('openai.index')">
+                                {{ __('OPEN AI') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('gpt.index')" :active="request()->routeIs('gpt.index')">
+                                {{ __('Chat GTP') }}
+                            </x-nav-link>
+                        </div>
+
+                    </div>
+
+
+
+
+
+                    <div class="flex-none">
+
+                        <a class="btn btn-ghost text-xl">daisyUI</a>
+                        <div class="dropdown dropdown-end">
+                        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                            <div class="indicator">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                            <span class="badge badge-sm indicator-item">8</span>
+                            </div>
+                        </div>
+                        <div
+                            tabindex="0"
+                            class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
+                            <div class="card-body">
+                            <span class="text-lg font-bold">8 Items</span>
+                            <span class="text-info">Subtotal: $999</span>
+                            <div class="card-actions">
+                                <button class="btn btn-primary btn-block">View cart</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="dropdown dropdown-end">
+                        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                            <div class="w-10 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                            </div>
+                        </div>
+                        <ul
+                            tabindex="0"
+                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li>
+                            <a class="justify-between">
+                                Profile
+                                <span class="badge">New</span>
+                            </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
             <!-- Settings Dropdown -->
